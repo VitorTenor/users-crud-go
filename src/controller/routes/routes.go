b89a911d86aa82d0d83e95b7d2ffaa387ec7d612
Vitor Tenório de Lima <vitortenorio35@gmail.com>
2023-08-09 19:34:03 -0300

    Feat => Finalizando token.

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -2,11 +2,12 @@ package routes
 
 import (
 	"github.com/VitorTenor/users-crud-go/src/controller/user"
+	"github.com/VitorTenor/users-crud-go/src/model"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(r *gin.RouterGroup, userController user.UserControllerInterface) {
-	r.GET("/getAllUsers", userController.FindAllUsers)
+	r.GET("/getAllUsers", model.VerifyTokenMiddleare, userController.FindAllUsers)
 	r.GET("/getUserById/:id", userController.FindUserById)
 	r.GET("/getUserByEmail/:email", userController.FindUserByEmail)
 	r.POST("/createUser", userController.CreateUser)
--- src/model/user_token_domain.go
+++ src/model/user_token_domain.go
@@ -1,9 +1,13 @@
 package model
 
 import (
+	"fmt"
+	"github.com/VitorTenor/users-crud-go/src/configuration/logger"
 	"github.com/VitorTenor/users-crud-go/src/configuration/rest_error"
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -30,4 +34,45 @@ func (ud *userDomain) GenerateToken() (string, *rest_error.Err) {
 	}
 
 	return tokenString, nil
+}
+
+func VerifyTokenMiddleare(c *gin.Context) {
+	secret := os.Getenv(JWT_SECRET_KEY)
+	tokenValue := RemoveBearerPrefix(c.Request.Header.Get("Authorization"))
+
+	token, err := jwt.Parse(tokenValue, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); ok {
+			return []byte(secret), nil
+		}
+		return nil, rest_error.NewBadRequestError("Invalid token", nil)
+	})
+	if err != nil {
+		errRest := rest_error.NewUnauthorizedError("Invalid token")
+		c.JSON(errRest.Code, errRest)
+		c.Abort()
+		return
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		errRest := rest_error.NewUnauthorizedError("Invalid token")
+		c.JSON(errRest.Code, errRest)
+		c.Abort()
+		return
+	}
+
+	userDomain := userDomain{
+		id:   claims["id"].(string),
+		name: claims["name"].(string),
+		age:  int8(claims["age"].(float64)),
+	}
+
+	logger.Info(fmt.Sprintf("User authenticated: %#v", userDomain))
+}
+
+func RemoveBearerPrefix(token string) string {
+	if strings.HasPrefix(token, "Bearer ") {
+		token = strings.TrimPrefix(token, "Bearer ")
+	}
+	return token
 }
